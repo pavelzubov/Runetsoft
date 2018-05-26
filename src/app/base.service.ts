9296a99f04e1edb470507bc2b00a81ec37f33407
pavelzubov <pasha224@yandex.ru>
2018-05-26 11:57:56 +0300

    Refactoring

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable, pipe} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
-import {Task,ServerResponse} from './types.factory';
+import {ServerResponse} from './types.factory';
 
 @Injectable()
 export class Base {
